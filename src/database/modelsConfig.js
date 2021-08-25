883d7699720bbe9ff75f8c91aaf61be85c89ff32
dizinnes <dizinnes228@gmail.com>
2021-08-25 20:24:25 +0600

    feat(db): Create env.example and connect really in modelsConfig.js

--- src/database/modelsConfig.js
+++ src/database/modelsConfig.js
@@ -1,7 +1,10 @@
+require('dotenv').config()
+
+
 module.exports.DATABASE_CONFIG = {
-  user: "postgres",
-  host: "localhost",
-  database: "database",
-  password: "postgres",
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
   port: 5432,
 };
